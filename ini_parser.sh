f8516f80b7c5650a06ed4e237dd037b2beab1fec
blackeye <private_blackeye+github@posteo.de>
2021-03-22 04:34:14 +0100

    finished ini_parser with git flavoured sed transformation

--- ini_parser.sh
+++ ini_parser.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# use this
 # Read and parse simple INI file
+# Stop even, if a command in a pipe fails.
+set -e -o pipefail
 
 TMP_FILE="tmp.ini"
+
+# will be added in mainfile deep_copy_submodules.sh 
+# source "log.sh"
+
 # preprocessing
 preprocessing() {
     
@@ -19,9 +26,13 @@ preprocessing() {
     ## \1 = first backreference (\n nth backreference), referred to the backreference group
     
     ## Does: standardize git .modules to a normal ini "[submodulename]"
-    filename="${1}"
-    echo "creating file that will be parsed"
-    sed 's/^\[.\+"\(.\+\)\"\]/[\1]/' ${filename} > ${TMP_FILE}
+    local filename="${1}"
+    info "INFO PARSING INI" "parsing ${filename} into standardized format ini file: ${TMP_FILE}"
+    
+    sed 's/^\[.\+"\(.\+\)\"\]/[\1]/' ${filename} | sed 's/\-/_/g' > ${TMP_FILE}
+    # clear invalid characters
+    
+    echo "true"
 }
 
 
@@ -66,27 +77,33 @@ get_ini_sections(){
     
 }
 
-if [ "$#" -eq "1" ] && [ -f "$1" ]; then
-    ret_value=$(preprocessing "$1")
-    if [ -f "${TMP_FILE}"] && [[ ret_value == "true" ]]; then
-        get_ini_sections "$filename"
-        
-        echo -n "Configuration description: "
-        if [ -n "${configuration_main["description"]}" ]; then
-            echo "${configuration_main["description"]}"
-        else
-            echo "missing"
+parse_ini() {
+    # input: ini file (with git flavour)
+    # output in global variables: parsed ini with $configuration_section[field]=value
+    if [ "$#" -eq "1" ] && [ -f "$1" ]; then
+        ret_value=$(preprocessing "$1")
+        if [[ -f "${TMP_FILE}" ]] && [[ ${ret_value} == "true" ]] ; then
+            
+            filename="${TMP_FILE}"
+            get_ini_sections "$filename"
+            
+            # echo -n "Configuration description: "
+            # if [ -n "${configuration_main["description"]}" ]; then
+            #     echo "${configuration_main["description"]}"
+            # #else
+            # echo "missing"
+            #fi
+            section_list=$(read_ini_sections "${filename}")
+
+            info "INFO - Segments of Ini " "$(declare -p sections)"
+            # for section in $(read_ini_sections "${filename}"); do
+            #     echo "[${section}]"
+            #     for key in $(eval echo $\{'!'configuration_${section}[@]\}); do
+            #         echo -e "  ${key} = $(eval echo $\{configuration_${section}[$key]\}) (access it using $(echo $\{configuration_${section}[$key]\}))"
+            #     done
+            # done
         fi
-        echo
-        
-        for section in $(read_ini_sections "${filename}"); do
-            echo "[${section}]"
-            for key in $(eval echo $\{'!'configuration_${section}[@]\}); do
-                echo -e "  ${key} = $(eval echo $\{configuration_${section}[$key]\}) (access it using $(echo $\{configuration_${section}[$key]\}))"
-            done
-        done
+    else
+        echo "missing INI file"
     fi
-    
-else
-    echo "missing INI file"
-fi
+}
